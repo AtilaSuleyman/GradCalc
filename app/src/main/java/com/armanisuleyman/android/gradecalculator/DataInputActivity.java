82b2a2a0870836809975fdc9bcbf430ba524476f
Atila Suleyman <Atila Suleyman>
2016-08-14 22:34:38 +0100

    First commit

---
+++ app/src/main/java/com/armanisuleyman/android/gradecalculator/DataInputActivity.java
@@ -0,0 +1,23 @@
+package com.armanisuleyman.android.gradecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class DataInputActivity extends AppCompatActivity {
+
+    //Define all the mark fields in java from XML
+    private EditText markField1 = (EditText)findViewById(R.id.mark1);
+    private EditText markField2 = (EditText)findViewById(R.id.mark2);
+    private EditText markField3 = (EditText)findViewById(R.id.mark3);
+    private EditText markField4 = (EditText)findViewById(R.id.mark4);
+    private EditText markField5 = (EditText)findViewById(R.id.mark5);
+    private EditText markField6 = (EditText)findViewById(R.id.mark6);
+    //Define all the percentage fields in java from XML
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_input);
+    }
+}
